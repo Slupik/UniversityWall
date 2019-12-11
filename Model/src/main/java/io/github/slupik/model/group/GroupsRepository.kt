8ae806c6844ec6e56c62d8cd3d945c9202e9c73f
Slupik <Slupik98@gmail.com>
2019-12-11 20:24:50 +0100

    feat: Logic for groups

--- Model/src/main/java/io/github/slupik/model/group/GroupsRepository.kt
+++ Model/src/main/java/io/github/slupik/model/group/GroupsRepository.kt
@@ -3,19 +3,25 @@
  * All rights reserved. No part of this application may be reproduced or be part of other software, without the prior written permission of the publisher. For permission requests, write to the author(WitasikSebastian@gmail.com).
  */
 
-package io.github.slupik.model.dagger
+package io.github.slupik.model.group
 
-import dagger.Module
+import io.reactivex.Completable
+import io.reactivex.Flowable
+import io.reactivex.Single
 
 /**
  * Created by Sebastian Witasik on 10.12.2019.
  * E-mail: SebastianWitasik@gmail.com
  * All rights reserved & copyright ©
  */
-@Module(
-    includes = [
-        MessagesModule::class,
-        GroupsModule::class
-    ]
-)
-class ModelModule
+interface GroupsRepository {
+
+    fun save(groups: List<Group>): Completable
+
+    fun fetchAll(): Flowable<List<Group>>
+
+    fun getAll(): Single<List<Group>>
+
+    fun deleteAll()
+
+}
---
+++ Model/src/main/java/io/github/slupik/model/group/GroupsSynchronizer.kt
@@ -0,0 +1,60 @@
+/*
+ * Copyright (c) 2019. by Sebastian Witasik
+ * All rights reserved. No part of this application may be reproduced or be part of other software, without the prior written permission of the publisher. For permission requests, write to the author(WitasikSebastian@gmail.com).
+ */
+
+package io.github.slupik.model.group
+
+import io.reactivex.BackpressureStrategy
+import io.reactivex.Completable
+import io.reactivex.Flowable
+import io.reactivex.Single
+import io.reactivex.subjects.PublishSubject
+import javax.inject.Inject
+import javax.inject.Singleton
+
+/**
+ * Created by Sebastian Witasik on 10.12.2019.
+ * E-mail: SebastianWitasik@gmail.com
+ * All rights reserved & copyright ©
+ */
+@Singleton
+class GroupsSynchronizer @Inject constructor(
+    private val downloader: GroupsDownloader,
+    private val repository: GroupsRepository
+) : GroupsProvider {
+
+    private val groupsPublisher: PublishSubject<List<Group>> = PublishSubject.create()
+    override val groupsEmitter: Flowable<List<Group>>
+        get() = groupsPublisher.toFlowable(BackpressureStrategy.BUFFER)
+
+    override val groups: Single<List<Group>>
+        get() = repository.getAll()
+
+    override fun refresh(): Completable =
+        downloader
+            .downloadGroups()
+            .saveAndEmitNewList()
+
+    private fun Single<List<Group>>.saveAndEmitNewList(): Completable =
+        this.flatMapCompletable { messages ->
+            repository
+                .save(messages)
+                .andThen(
+                    Completable.defer {
+                        emitNewList()
+                    }
+                )
+        }
+
+    private fun emitNewList(): Completable =
+        repository
+            .getAll()
+            .doOnSuccess { refreshedList ->
+                groupsPublisher.onNext(refreshedList)
+            }.flatMapCompletable {
+                Completable.complete()
+            }
+
+}
+
