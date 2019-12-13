826a6bd94830795cd54bbe095cbde192ccfe79a0
Slupik <Slupik98@gmail.com>
2019-12-13 03:58:28 +0100

    feat: Deleted work manager

--- app/src/main/java/io/github/slupik/universitywall/application/MyApplication.kt
+++ app/src/main/java/io/github/slupik/universitywall/application/MyApplication.kt
@@ -8,14 +8,10 @@ package io.github.slupik.universitywall.application
 import android.app.Application
 import android.content.Intent
 import androidx.core.content.ContextCompat
-import androidx.work.*
 import io.github.slupik.universitywall.background.service.SynchronizingService
-import io.github.slupik.universitywall.background.syncing.MESSAGES_SYNCING_WORK_NAME
-import io.github.slupik.universitywall.background.syncing.MessagesSyncingWorker
 import io.github.slupik.universitywall.dagger.ApplicationComponent
 import io.github.slupik.universitywall.dagger.ContextModule
 import io.github.slupik.universitywall.dagger.DaggerApplicationComponent
-import java.util.concurrent.TimeUnit
 
 
 /**
@@ -34,7 +30,6 @@ class MyApplication : Application() {
                 ContextModule(this)
             )
             .build()
-        startBackgroundMessagesSyncing()
         startSyncing()
         super.onCreate()
     }
@@ -42,29 +37,6 @@ class MyApplication : Application() {
     private fun startSyncing() {
         val serviceIntent = Intent(this, SynchronizingService::class.java)
         ContextCompat.startForegroundService(this, serviceIntent)
-    }
-
-    private fun startBackgroundMessagesSyncing() {
-        val constraints = Constraints.Builder()
-            .setRequiredNetworkType(NetworkType.CONNECTED)
-            .build()
-
-        val work =
-            PeriodicWorkRequest
-                .Builder(
-                    MessagesSyncingWorker::class.java,
-                    15,
-                    TimeUnit.MINUTES
-                )
-                .setConstraints(constraints)
-                .build()
-
-        val workManager = WorkManager.getInstance(applicationContext)
-        workManager.enqueueUniquePeriodicWork(
-            MESSAGES_SYNCING_WORK_NAME,
-            ExistingPeriodicWorkPolicy.REPLACE,
-            work
-        )
     }
 
 }
